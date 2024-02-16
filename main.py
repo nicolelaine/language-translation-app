51b5c58d0a2deb6713e9eddf07918c1737fd696c
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-02-16 02:03:32 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import csv
+
+translations = {}
+
+with open("translations.csv", "r") as words:
+  reader = csv.DictReader(words, delimiter=",")
+  for line in reader:
+    english = line["English"].lower()
+    spanish = line["Spanish"].lower()
+    french = line["French"].lower()
+    translations[english] = [spanish, french]
+
+done = False
+
+print('Type "done" at any time to exit')
+
+while not done:
+  word = input("Enter an English word to translate: ")
+  word = word.lower()
+  if word == "done":
+    done = True
+  elif word in translations:
+    print(translations[word])
+  else:
+    print("Sorry, I don't know that word")
